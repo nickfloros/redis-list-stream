f267354776de84199cbbbcaa0bd1ea9d4628fe74
Nick Floros <nickfloros@gmail.com>
2023-05-10 22:24:01 +0100

    adding basic redis connection client and unit tests..

--- spec/src/redis-client.connection.spec.js
+++ spec/src/redis-client.connection.spec.js
@@ -4,15 +4,45 @@ describe('redis-client', () => {
 	const redis = require('redis');
 
 	beforeEach(()=>{
-		spyOn(redis,'createClient').and.callFake(()=>{
-			return 'y';
+		spyOn(redis,'createClient').and.callFake(async ()=>{
+			return Promise.resolve('y');
 		});
 
 	});
 
-	it('use the client passed  ..', () => {
-		const mockClient =RedisClient.create({client :'x'});
-		expect(mockClient).toBe('y');
+	it('should throw an exception that host is missing.', async (done) => {
+		try {
+			await RedisClient.create({});
+		}
+		catch (err) {
+			expect(err.message).toContain('missing host')
+			done();
+		}
+	});
+	it('should throw an exception that port is missing.', async (done) => {
+		try {
+			await RedisClient.create({host:'x'});
+		}
+		catch (err) {
+			expect(err.message).toContain('missing port')
+			done();
+		}
+	});
+
+	it('should throw an exception that password is missing.', async (done) => {
+		try {
+			await RedisClient.create({host:'x', port : 1});
+		}
+		catch (err) {
+			expect(err.message).toContain('missing password')
+			done();
+		}
+	});
+
+	it('should return a client', async (done) => {
+		const client = await RedisClient.create({host:'x', port : 1, password:'x'});
+		expect(client).toBe('y');
+		done();
 	});
 
 });
